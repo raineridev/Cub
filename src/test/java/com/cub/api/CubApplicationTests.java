34a791a7ee61c50aa4ea641078e5ac47d2e60f38
Marcelo Raineri <mraineridev@gmail.com>
2025-06-23 01:04:17 -0300

    start project

---
+++ src/test/java/com/cub/api/CubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
